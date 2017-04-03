f28803759ece50d9fe536497df9761d51ba08102
Demid Miroshnichenko <iamdemid@gmail.com>
2017-04-03 21:58:45 +0300

    фикс js + мелкие правки

--- js/app.js
+++ js/app.js
@@ -3,6 +3,8 @@
   var menuMobile = document.querySelector(".main-menu");
   var menuClose = menuMobile.querySelector(".main-menu__cross-button");
 
+  menuMobile.classList.remove(".main-menu--no-js");
+
   menuOpen.addEventListener("click", function(event) {
     event.preventDefault();
     menuMobile.classList.add("main-menu__modal-content-show");
